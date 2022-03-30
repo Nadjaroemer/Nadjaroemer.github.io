95801d56bbcf95ceb1ffb8575d8870e20de33340
Nadjaroemer <nadja.kuti@gmail.com>
2022-03-30 12:18:26 +0200

    Update docusaurus config with repository values

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,9 +13,9 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'nadjaroemer', 
+  projectName: 'nadjaroemer.github.io', 
+  trailingSlash: false,
   presets: [
     [
       'classic',
